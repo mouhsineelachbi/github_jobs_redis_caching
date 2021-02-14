0764252392a7ad46ce9ce3a11ac091e50be2ed5a
mouhsineelachbi <mouhsineelachbi@gmail.com>
2021-02-14 04:06:58 +0100

    fetching api

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
-const express   =   require('express');
-const redis     =   require('redis');
-const   app     =   express();
+const express = require('express');
+const redis = require('redis');
+const axios = require('axios');
+const   app = express();
 
 
-
+app.get("/jobs", async (req, res) => {
+    const searchTerm = req.query.search;
+    try {
+        const jobs = await axios.get(`https://jobs.github.com/positions.json?search=${searchTerm}`);
+        res.status(200).send({
+            jobs: jobs.data,
+        });	
+    } catch(err) {
+        res.status(500).send({message: err.message});
+    }
+});
 
 app.listen(3000, ()=>console.log(`Listening on port 3000`));
